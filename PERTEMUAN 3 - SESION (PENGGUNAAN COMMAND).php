8e67503a9b876ff9f2bc00a035955ff0ba86fcb5
Christya Wahyu Widyaningrum <79434963+Christyawahyu@users.noreply.github.com>
2021-03-05 22:24:59 +0700

    Add files via upload
    PERTEMUAN KE 3

---
+++ PERTEMUAN 3 - SESION (PENGGUNAAN COMMAND).php
@@ -0,0 +1,13 @@
+<?php
+// ini adalah komentar yang tidak dieksekusi oleh program
+
+echo " Saya sedang belajar berhitung menggunakan php, Berapakah hasil penjumlahan 3+4 <br>";
+/* Ini adalah komentar bagian kedua yang menggunakan enter.
+kita bisa menulis banyak catatan disini 
+test
+test
+qwweertyuiop
+*/
+
+echo "Baris Kedua";
+?>
